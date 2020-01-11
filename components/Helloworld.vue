2d28ed21358d5f53cb398fc8d0b51317184466a1
ankitjain28may <ankitjain28may77@gmail.com>
2020-01-11 21:01:51 +0530

    Added component in bit with version

---
+++ components/Helloworld.vue
@@ -0,0 +1,8 @@
+<template>
+<p>Helloworld Updated</p>
+</template>
+<script>
+export default {
+  name: 'Helloworld'
+}
+</script>
---
+++ index.vue
@@ -0,0 +1,14 @@
+<template>
+	<Helloworld/>
+</template>
+
+<script>
+	import Vue from 'vue';
+	import Helloworld from '@bit/ankitjain28may.module.components';
+  Vue.component(Helloworld.name, Helloworld)
+	export default {}
+</script>
+
+<style scoped>
+
+</style>
